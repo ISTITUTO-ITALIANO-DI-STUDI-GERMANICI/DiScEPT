94400b963ef1259d97b39aaa4ba67304c709abc9
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2024-06-03 19:16:09 +0200

    Prototype of the onboarding

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { CookiesProvider } from 'react-cookie';
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </StrictMode>,
 );
