8c024632675d0e69d0fc4e9283fc428518f236c6
Andrea Marchesini <amarchesini@mozilla.com>
2024-06-04 06:45:49 +0200

    Remove cookies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { CookiesProvider } from "react-cookie";
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </StrictMode>,
 );
