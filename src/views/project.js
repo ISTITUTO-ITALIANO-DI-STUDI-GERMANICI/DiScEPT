0d44f840ebdc2ca2c0a1affd4dffce9fafceb2a4
Tiziana Mancinelli <18259343+tmancinelli@users.noreply.github.com>
2025-06-09 22:56:42 +0200

    Improve project view with multi-value fields

--- src/views/project.js
+++ src/views/project.js
@@ -3,44 +3,55 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Chip from "@mui/material/Chip";
 
 import Title from '../components/title.js';
 import data from "../Data.js";
 
 const fields = [
   { id: "title", name: "Project title", required: true },
-  {
-    id: "authors",
-    name: "Authors (one per line)",
-    required: true,
-    multiline: true,
-  },
-  {
-    id: "resps",
-    name: "Resps (one per line)",
-    required: false,
-    multiline: true,
-  },
-  {
-    id: "pubStatement",
-    name: "Publication Statement",
-    required: false,
-    multiline: true,
-  },
+  { id: "authors", name: "Authors", required: true, multiple: true },
+  { id: "resps", name: "Responsible persons", multiple: true },
+  { id: "pubStatement", name: "Publication Statement", multiline: true },
 ];
 
 function ProjectView() {
   const [validForm, setValidForm] = React.useState(false);
-  const [values, setValues] = React.useState(data.project);
+  const [values, setValues] = React.useState({
+    title: data.project.title || "",
+    authors: Array.isArray(data.project.authors)
+      ? data.project.authors
+      : data.project.authors
+      ? data.project.authors.split("\n")
+      : [],
+    resps: Array.isArray(data.project.resps)
+      ? data.project.resps
+      : data.project.resps
+      ? data.project.resps.split("\n")
+      : [],
+    pubStatement: data.project.pubStatement || "",
+  });
 
   const handleChange = (e, id) => {
     e.preventDefault();
-    const validity = document.forms[0].checkValidity();
-    setValidForm(validity);
 
-    values[id] = e.target.value;
-    setValues(values);
+    setValues((prev) => ({ ...prev, [id]: e.target.value }));
   };
+
+  const handleMultiChange = (id, newValue) => {
+    setValues((prev) => ({ ...prev, [id]: newValue }));
+  };
+
+  React.useEffect(() => {
+    const isValid = fields.every((field) => {
+      const val = values[field.id];
+      if (!field.required) return true;
+      if (field.multiple) return val && val.length > 0;
+      return val !== "" && val !== undefined;
+    });
+    setValidForm(isValid);
+  }, [values]);
 
   const handleClick = () => {
     data.project = values;
@@ -53,14 +64,41 @@ function ProjectView() {
       <Box id="project-fields">
         {fields.map((field, index) => (
           <Box key={index} component="span" display="block" gap={4} p={2}>
-            <TextField
-              label={field.name}
-              required={field.required || false}
-              multiline={field.multiline || false}
-              onChange={(e) => handleChange(e, field.id)}
-              sx={{ width: 500 }}
-              defaultValue={values[field.id] || ""}
-            />
+            {field.multiple ? (
+              <Autocomplete
+                multiple
+                freeSolo
+                value={values[field.id]}
+                onChange={(e, newValue) => handleMultiChange(field.id, newValue)}
+                options={[]}
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip
+                      variant="outlined"
+                      label={option}
+                      {...getTagProps({ index })}
+                    />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label={field.name}
+                    required={field.required || false}
+                  />
+                )}
+                sx={{ width: 500 }}
+              />
+            ) : (
+              <TextField
+                label={field.name}
+                required={field.required || false}
+                multiline={field.multiline || false}
+                onChange={(e) => handleChange(e, field.id)}
+                sx={{ width: 500 }}
+                defaultValue={values[field.id] || ""}
+              />
+            )}
           </Box>
         ))}
       </Box>
